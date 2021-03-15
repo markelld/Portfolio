355784cb2ab9fdd792e3ab58fe46980cd5c67910
“markelld” <markelldehaney@gmail.com>
2021-03-15 14:54:42 -0500

    css edits

--- src/screens/About/About.jsx
+++ src/screens/About/About.jsx
@@ -10,8 +10,8 @@ const About = () => {
         <div className="about-main"> 
           <div className="info"> 
             <h1 className="about-tite">About Me</h1> 
-            <p className="paragraph">I am a software engineer that has a hunger to learn new ways to innovate and bring my ideas to life. I see Software engineeing as an outlet for me to solve issues and create a more modern approach to problem solving. Prior to developing I worked in the hospitality industry with a formal education in mechanical engineering and Degree in Marketing.
-               My journey to software engineering started with self study followed by attending General Assembly's Full Stack Engineering Program.The program included over 400 hours of professional training over 12 weeks. Utilizing a hands-on approach to design and build full-stack web applications with Git,HTML, CSS,Javascript, React, Express, MongoDB, SQL, PostgreSQL,Ruby and Rails. 
+            <p className="paragraph">Developing software has created a hunger to learn, creating new ways for me to innovate. I see Software engineeing as an outlet to express myself, it kind of feels like its art. Prior to developing I worked in the hospitality industry as a Bartender.  
+               My journey to software engineering started with self study followed by attending General Assembly's Full Stack Engineering Program.The program included over 400 hours of professional training over 12 weeks. Utilizing a hands-on approach to design and building full-stack web applications with Git,HTML, CSS,Javascript, React, Express, MongoDB, SQL, PostgreSQL and Ruby on Rails. 
             </p>
           </div>
           <a
--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -1,107 +1,145 @@
-import "./Home.css"; 
-import Layout from "../../components/Layout/Layout";  
-import Button from 'react-bootstrap/Button'  
-import { Link } from "react-router-dom"; 
-
+import "./Home.css";
+import Layout from "../../components/Layout/Layout";
+import Button from "react-bootstrap/Button";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div> 
-      <Layout> 
-        <div className="main"> 
-          <div className="first-block">  
-             <div className="statement">
-              <h1 className="name-heading">Hey, I'm Markell.</h1> 
-              <p className="brand-statement">My current passion is to express my creativity through code, while constantly evolving through learning.</p> 
+    <div>
+      <Layout>
+        <div className="main">
+          <div className="first-block">
+            <div className="statement">
+              <h1 className="name-heading">Hey, I'm Markell.</h1>
+              <p className="brand-statement">
+                I'm consantly trying to learn something new to execute my ideas.
+              </p>
               <Link to="/about">
-                <Button variant="danger" className="about-me-button">About Me</Button>
-              </Link>
-            </div> 
-          </div> 
-          <div className="project-heading-div">
-          <h1 className="projects-heading">Projects</h1>
+                <Button variant="danger" className="about-me-button">
+                  About Me
+                </Button>
+              </Link>  
+              <a
+                target="_blank"
+                alt=""
+                href="https://docs.google.com/document/d/1axaqKrqbP9x1_yrgSuJ0cvta30rMCv2zfxrFHY6JzPI/edit?usp=sharing"
+              >
+                <Button variant="danger" className="about-me-button">
+                  Resume
+                </Button>
+              </a>
+            </div>
           </div>
-          <div className="second-block">   
-            <div className="project">
-                <div>
-                  <img src="CCTD.png" className="project-image" /> 
-                </div>
-                <h3 className="project-name">CCTD</h3> 
-                <p className="project-des">Covid Cases and Testing Data is a application to search daily covid rates in Chicago along with testing locations by zipcode. 
-                </p>  
-                <a href="https://github.com/markelld/Covid-Data" target="_blank" alt="">
-                  <button className="project-button">View Code</button> 
-                </a> 
-                <a href="https://cctd.netlify.app/" target="_blank" alt="">
-                  <button className="project-button">View App</button>
-                </a>
-            </div> 
-            <div className="project"> 
-                <div>
-                  <img src="SS.png" className="project-image"/> 
-                </div>
-                <h3 className="project-name">Shaken or Stirred?</h3> 
-                <p className="project-des">Created with React and Airtable, the app consists of classic cocktail recipes along with creations from fellow bartenders from Chicago.</p> 
-                <a href="https://github.com/markelld/ShakenorStirred" target="_blank" alt="">
-                  <button className="project-button">View Code</button>  
-                </a> 
-                <a href="https://shorst.netlify.app/" target="_blank" alt="">
-                  <button className="project-button">View App</button>
-                </a>
-            </div> 
-            <div className="project"> 
-              <div>
-                <img src="Linterna.png" className="project-image"/> 
+          <div className="project-heading-div">
+            <h1 className="projects-heading">Skills</h1> 
+          </div>
+          <div className="">
+            <div className="third-block">
+              <div className="front-end">
+                <h3 className="skills-subheading">Front-end:</h3>
+                <ul>
+                  <li>HTML,CSS,Javascript</li>
+                  <li>React for single page applications</li>
+                  <li>Axios to utilize Rest Api's</li> 
+                  <li>Bootstrap,Material UI</li>
+                </ul>
               </div>
-              <h3 className="project-name">Linterna</h3> 
-              <p className="project-des">Collaborative project made with React,Express and MongoDB.The project was made during 5 day sprint with github to manage workflow.</p>  
-              <a href="https://github.com/hrisso/lighting-project" target="_blank" alt="">
-                <button className="project-button">View Code</button> 
-              </a> 
-              <a href="https://linterna-lamps.netlify.app/" target="_blank" alt="">
+              <div className="back-end">
+                <h3 className="skills-subheading">Back-end:</h3>
+                <ul>
+                  <li>MongoDB</li>
+                  <li>Express</li>
+                  <li>SQL,PostgreSQL</li>
+                  <li>Ruby & Ruby on Rails</li>
+                </ul>
+              </div>
+              <div className="development-tools">
+                <h3 className="skills-subheading">Tools for development:</h3>
+                <ul>
+                  <li>Postman</li>
+                  <li>Compass</li>
+                  <li>Github</li>
+                  <li>Chrome Developer Tools</li>
+                  <li>VSCode</li>
+                </ul>
+              </div> 
+            </div> 
+          </div>
+          <div className="project-heading-div">
+            <h1 className="projects-heading">Projects</h1>
+          </div>
+          <div className="second-block">
+            <div className="project">
+              <div>
+                <img src="CCTD.png" className="project-image" />
+              </div>
+              <h3 className="project-name">CCTD</h3>
+              <p className="project-des">
+                Covid Cases and Testing Data is a application to search daily
+                covid rates in Chicago along with testing locations by zipcode.
+              </p>
+              <a
+                href="https://github.com/markelld/Covid-Data"
+                target="_blank"
+                alt=""
+              >
+                <button className="project-button">View Code</button>
+              </a>
+              <a href="https://cctd.netlify.app/" target="_blank" alt="">
                 <button className="project-button">View App</button>
               </a>
-            </div> 
-          </div>   
-          <div>
-            <h1 className="skills-heading">Skills</h1> 
-          </div>
-          <div className="third-block">   
-            <div className="front-end"> 
-              <h3 className="skills-subheading">Front-end:</h3> 
-              <ul>
-                <li>HTML,CSS,Javascript</li> 
-                <li>React for single page applications</li> 
-                <li>Axios to utilize Rest Api's</li> 
-              </ul>  
-            </div> 
-            <div className="back-end">
-              <h3 className="skills-subheading">Back-end:</h3> 
-              <ul>
-                <li>MongoDB</li> 
-                <li>Express</li> 
-                <li>SQL,PostgreSQL</li> 
-                <li>Ruby & Ruby on Rails</li>
-              </ul>
-            </div> 
-            <div className="development-tools"> 
-              <h3 className="skills-subheading">Tools for development:</h3>  
-              <ul>
-                <li>Postman</li> 
-                <li>Compass</li> 
-                <li>Github</li> 
-                <li>Chrome Developer Tools</li> 
-                <li>VSCode</li>
-              </ul>
-            </div> 
+            </div>
+            <div className="project">
+              <div>
+                <img src="SS.png" className="project-image" />
+              </div>
+              <h3 className="project-name">Shaken or Stirred?</h3>
+              <p className="project-des">
+                Created with React and Airtable, the app consists of classic
+                cocktail recipes along with creations from fellow bartenders
+                from Chicago.
+              </p>
+              <a
+                href="https://github.com/markelld/ShakenorStirred"
+                target="_blank"
+                alt=""
+              >
+                <button className="project-button">View Code</button>
+              </a>
+              <a href="https://shorst.netlify.app/" target="_blank" alt="">
+                <button className="project-button">View App</button>
+              </a>
+            </div>
+            <div className="project">
+              <div>
+                <img src="Linterna.png" className="project-image" />
+              </div>
+              <h3 className="project-name">Linterna</h3>
+              <p className="project-des">
+                Collaborative project made with React,Express and MongoDB.The
+                project was made during 5 day sprint with github to manage
+                workflow.
+              </p>
+              <a
+                href="https://github.com/hrisso/lighting-project"
+                target="_blank"
+                alt=""
+              >
+                <button className="project-button">View Code</button>
+              </a>
+              <a
+                href="https://linterna-lamps.netlify.app/"
+                target="_blank"
+                alt=""
+              >
+                <button className="project-button">View App</button>
+              </a>
+            </div>
           </div>
         </div>
       </Layout>
     </div>
+  );
+};
 
-  )
-} 
-
-
-export default Home; 
-            
+export default Home;
